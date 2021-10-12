a40b04777733a25c885baa9a9a725c7ed8465b9c
Juan Calzadilla <juancalza91@gmail.com>
2021-10-12 20:03:23 +0200

    Sweetalert Added

--- src/actions/cart-actions.js
+++ src/actions/cart-actions.js
@@ -1,10 +1,19 @@
 import { types } from "../types/types";
+import Swal from 'sweetalert2'
 
 
 
 
 export const addNewItem = (item) =>{
  return(dispatch)=>{
+    Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'Item has been added',
+        text:item.product_name,
+        showConfirmButton: false,
+        timer: 1500
+      })
     dispatch(newItem(item))
 }
 
